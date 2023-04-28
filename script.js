41c95ef71e19bce276128e5778476084a9dba07a
Gavo51 <gavo.gs64@gmail.com>
2023-04-28 11:00:32 -0400

    Rework renderBoard method

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 const gameBoard = (() => {
-  const boardCells = document.querySelectorAll(".board-cell");
-  const board = [
+  const boardState = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
-  let x = 0;
-  let y = 0;
 
   const saveCoordinates = (e) => {
-    x = e.target.dataset.x;
-    y = e.target.dataset.y;
+    let x = e.target.dataset.x;
+    let y = e.target.dataset.y;
 
-    storePLay();
-    fillCell(e.target);
+    storePlay(x, y);
+    renderBoard();
   };
 
   const addEvents = (() => {
-    boardCells.forEach((cell) => {
+    document.querySelectorAll(".board-cell").forEach((cell) => {
       cell.addEventListener("click", saveCoordinates);
     });
   })();
 
-  const storePLay = () => {
-    board[x][y] = "X";
+  const storePlay = (x, y) => {
+    boardState[x][y] = "X";
   };
 
-  const fillCell = (clickedCell) => {
-    clickedCell.textContent = "X";
+  const renderBoard = () => {
+    for (let i = 0; i < boardState.length; i++) {
+      for (let j = 0; j < boardState[i].length; j++) {
+        if (boardState[i][j] !== 0) {
+          document.querySelector(`[data-x="${i}"][data-y="${j}"]`).textContent =
+            boardState[i][j];
+        }
+      }
+    }
   };
 
-  return { board };
+  return { boardState };
 })();
-
-console.log(gameBoard.board);
