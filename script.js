22f10af28093ed9f4ff61b8e272d35b3b7c2be32
Gavo51 <gavo.gs64@gmail.com>
2023-05-01 11:40:37 -0400

    create playTurn  module

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 const gameBoard = (() => {
   const boardState = [
-    [0, 0, 0],
+    [0, "X", 0],
     [0, 0, 0],
     [0, 0, 0],
   ];
-
-  const saveCoordinates = (e) => {
-    let x = e.target.dataset.x;
-    let y = e.target.dataset.y;
-
-    storePlay(x, y);
-    renderBoard();
-  };
-
-  const addEvents = (() => {
-    document.querySelectorAll(".board-cell").forEach((cell) => {
-      cell.addEventListener("click", saveCoordinates);
-    });
-  })();
 
   const storePlay = (x, y) => {
     boardState[x][y] = "X";
   };
 
-  const renderBoard = () => {
+  const render = () => {
     for (let i = 0; i < boardState.length; i++) {
       for (let j = 0; j < boardState[i].length; j++) {
         if (boardState[i][j] !== 0) {
@@ -34,5 +20,37 @@ const gameBoard = (() => {
     }
   };
 
-  return { boardState };
+  return { storePlay, render, boardState };
 })();
+
+const playTurn = (() => {
+  const saveCoordinates = (e) => {
+    let x = e.target.dataset.x;
+    let y = e.target.dataset.y;
+
+    gameBoard.storePlay(x, y, player);
+    gameBoard.render();
+  };
+
+  const addEvents = (() => {
+    document.querySelectorAll(".board-cell").forEach((cell) => {
+      cell.addEventListener("click", saveCoordinates);
+    });
+  })();
+
+  return {};
+})();
+
+/* const player = () => {
+  let name = "Gabriel";
+  let symbol = "X";
+  let score = 0;
+
+  const getPlayerSymbol = () => {
+    this.symbol;
+  };
+
+  return { getPlayerSymbol };
+};
+
+console.log(player.getPlayerSymbol); */
