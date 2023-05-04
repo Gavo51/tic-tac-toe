910327e428c79ae9fd6d128372ca3bac27da8449
Gavo51 <gavo.gs64@gmail.com>
2023-05-04 11:15:47 -0400

    Add some explanatory comments

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ const gameBoard = (() => {
 
   const restart = () => {};
 
+  // Updates the board using the coordinates and the mark of the player passed through setMark method
+  // setMark method is triggered by the playRound.initialized method
   const update = (x, y, mark) => {
     board[x][y] = mark;
   };
@@ -31,6 +33,7 @@ const gameBoard = (() => {
   };
 
   const render = () => {
+    // Iterate over the board array and use a querySelector with data-attributes to link it to the DOM
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
         if (board[i][j] !== 0) {
@@ -46,10 +49,14 @@ const gameBoard = (() => {
 
 const playTurn = (() => {
   const setMark = (e) => {
+    // Get the coordinates using html data attributes (data-x, data-y)
     let x = e.target.dataset.x;
     let y = e.target.dataset.y;
 
+    // Call validation method to check if the cell is filled
     if (boardCheck.validateCell(x, y)) {
+      // Call turnControl to check the current turn and pick a player from the list using the index
+      // Then call board update method and pass the coordinates and the picked player's mark
       switch (turnControl.getCurrentTurn()) {
         case 1:
           gameBoard.update(x, y, playerList[1].symbol);
@@ -74,6 +81,7 @@ const playTurn = (() => {
 const turnControl = (() => {
   let currentTurn = 1;
 
+  // Change between 1 and 2 when called
   const changeTurn = () => {
     switch (currentTurn) {
       case 1:
